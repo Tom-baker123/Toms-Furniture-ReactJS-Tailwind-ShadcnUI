efa41a263e22873d49a0d6772f65942493638d0c
tom <119601473+Tom-baker123@users.noreply.github.com>
2025-05-20 00:26:47 +0700

    Cart-Modal-Content
    Sửa nội dung cart

--- src/components/tailwind-custom/ButtonHovCT.jsx
+++ src/components/tailwind-custom/ButtonHovCT.jsx
@@ -15,7 +15,17 @@ const ButtonHovCT = ({
             onClick={onClick}
             className={`group relative overflow-hidden rounded-full px-6 py-2 font-semibold ${bgColor} ${textColor} ${border ? `border ${textColor.replace("text-", "border-")}` : ""} transition-colors duration-300 ease-in-out ${className} `}
         >
-            <span className={`relative !z-10 transition-colors duration-300 group-hover:text-black`}>{children}</span>
+            <span
+                className={`relative !z-10 whitespace-nowrap transition-colors duration-300 ${
+                    hoverTextColor
+                        ? hoverTextColor.startsWith("group-hover:")
+                            ? hoverTextColor
+                            : `group-hover:${hoverTextColor}`
+                        : "group-hover:text-black"
+                }`}
+            >
+                {children}
+            </span>
             <span
                 className={`absolute inset-0 ${hoverBgColor} !z-0 -translate-x-full transform transition-transform duration-300 ease-in-out group-hover:translate-x-0`}
             ></span>
