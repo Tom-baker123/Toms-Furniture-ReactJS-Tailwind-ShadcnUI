fc00f6c68f62ec1e0e848134f5d10bdd1e2be538
tom <119601473+Tom-baker123@users.noreply.github.com>
2025-05-23 10:07:54 +0700

    Products-List-Category
    Tạo danh sách product dạng sticky nhưng chưa sửa bảng filter

--- src/components/Home/ProductCategoryComponents/BannerProducts.jsx
+++ src/components/Home/ProductCategoryComponents/BannerProducts.jsx
@@ -3,17 +3,19 @@ import React from "react";
 const BannerProducts = ({ className, children }) => {
     return (
         <div className={className}>
-            <div className="grid grid-cols-2 overflow-hidden rounded-lg">
+            <div className="grid grid-cols-1 overflow-hidden rounded-lg md:grid-cols-2">
                 <img
                     src="/img/ProductsCategory/collection-banner-1.png"
                     alt=""
-                    className=""
+                    className="order-last md:order-first"
                 />
-                <div className="bg-red-100 flex justify-center items-center">
-                  <div className=" w-3/4 flex flex-col gap-2">
-                    <h2 className="font-bold text-3xl">Shop All</h2>
-                    <p className="font-semibold text-gray-500">Browse our complete collection of furniture and decor to find everything you need for your perfect home.</p>
-                  </div>
+                <div className="flex items-center justify-center bg-red-100">
+                    <div className="flex flex-col gap-2 px-5 py-5 md:w-3/4">
+                        <h2 className="text-3xl font-bold">Shop All</h2>
+                        <p className="text-sm font-semibold text-gray-500">
+                            Browse our complete collection of furniture and decor to find everything you need for your perfect home.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
